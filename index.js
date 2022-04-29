969a165a4f083a2a853ef34ad1470f8d179e3a9c
Virag Kormoczy <vkormoczy@gmail.com>
2022-04-29 19:21:21 +0300

    Add clock functionality

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-setInterval(setClock, 1000);
+const hourHandle = document.querySelector("[data-hour]");
+const minuteHandle = document.querySelector("[data-minute]");
+const secondHandle = document.querySelector("[data-second]");
 
-const hourHandle = document.querySelector('[data-hour]');
-const hourHandle = document.querySelector('[data-hour]');
-const hourHandle = document.querySelector('[data-hour]');
-
-const setClock() {
+const setClock = () => {
   const currentDate = new Date();
   const seconds = currentDate.getSeconds() / 60;
   const minutes = (seconds + currentDate.getMinutes()) / 60;
   const hour = (minutes + currentDate.getHours()) / 12;
-}
+  setRotation(secondHandle, seconds);
+  setRotation(minuteHandle, minutes);
+  setRotation(hourHandle, hour);
+};
+
+const setRotation = (element, rotation) => {
+  element.style.setProperty("--rotation", rotation * 360);
+};
+
+setInterval(setClock, 1000);
